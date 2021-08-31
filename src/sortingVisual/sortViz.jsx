428360c0cb7fe44d439cbd59e67cb76601a9c198
Manjot <69727541+mickee00000@users.noreply.github.com>
2021-09-01 00:19:14 +0530

    meri branch me pehla change

--- src/sortingVisual/sortViz.jsx
+++ src/sortingVisual/sortViz.jsx
@@ -4,14 +4,13 @@ import './sortViz.css';
 export default class SortingVisualizer extends React.Component{
     constructor(props){
         super(props);
-
         this.state = {unsortedArray: []}; // So, whenever our react app starts, this array get initialized.
     }
 
     resetArray() {
         const unsortedArray = []; 
-        for (let i = 0; i < 200; i++){ // Literally iterate through 100 random values and store them in the array intialized in the array.
-            unsortedArray.push(Math.floor(Math.random()*(700 - 5 + 1)));
+        for (let i = 0; i < 60; i++){ // Literally iterate through 100 random values and store them in the array intialized in the array.
+            unsortedArray.push(Math.floor(Math.random()*(650  + 1) + 60));
         }
         this.setState({unsortedArray})
     }
@@ -25,14 +24,14 @@ export default class SortingVisualizer extends React.Component{
         return (
             <div className="array-box">
             {
-                unsortedArray.map((value, idx) => (
-                    <div className="array-bar" key = {idx} style={{height: `${value}px`}}></div>
-                ))
+                unsortedArray.map(
+                    (value, idx) => (<div className="array-bar" key = {idx} style={{height: `${value}px`}}></div>)
+                )
             }
             <button onClick={() => this.resetArray()}>Generate New Array</button>
             </div>
         );
     }
+};
 
-   
-}
+
