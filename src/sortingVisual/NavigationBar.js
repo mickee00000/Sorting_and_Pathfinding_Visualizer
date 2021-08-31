9400e5abfffc5c667e882f1840d7c5459c170dca
be-jojo <sjojo4715@gmail.com>
2021-09-01 00:54:45 +0530

    some change

--- src/sortingVisual/NavigationBar.js
+++ src/sortingVisual/NavigationBar.js
@@ -1,31 +1,32 @@
 import React from 'react';
 
-import './sortViz.css';
+import "./ArrayChart.css";
+import {insertionSort} from './sortAlgorithms/insertion';
 
 function NavigationBar() {
   return (
     <nav>
       <ul>
-        <li id='shortpath'>
+        <li id='shortpath' className='items'>
           <button className='buttons'>Shortest Path</button>
         </li>
         <li className='items'>
-          <button className='buttons'>Change Size</button>
+          <button className='buttons'>Merge sort</button>
         </li>
         <li className='items'>
           <button className='buttons'>Selection Sort</button>
         </li>
         <li className='items'>
-          <button className='buttons'>Change Array</button>
+          <button className='buttons' onClick={insertionSort}>Insertion Sort</button>
         </li>
         <li className='items'>
-          <button className='buttons'>Change Array</button>
+          <button className='buttons'>Bubble Sort</button>
         </li>
         <li className='items'>
-          <button className='buttons'>Change Array</button>
+          <button className='buttons'>Quick sort</button>
         </li>
         <li className='items'>
-          <button className='buttons'>Change Array</button>
+          <button className='buttons'>Shell sort</button>
         </li>
       </ul>
     </nav>
