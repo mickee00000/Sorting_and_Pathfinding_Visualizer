1508abfc44dad72b34511c2abe6c58748daf6cf0
be-jojo <67816573+be-jojo@users.noreply.github.com>
2021-10-01 20:04:34 +0530

    Update ArrayChart.jsx

--- src/sortingVisual/ArrayChart.jsx
+++ src/sortingVisual/ArrayChart.jsx
@@ -1,47 +1,28 @@
-import React from "react";
-import "./ArrayChart.css";
+import React from 'react';
 
-import { genrateRandomArray } from "./sortAlgorithms/array";
+import './ArrayChart.css'
+import { useGlobalContext } from './context';
+import {createRandomArray} from './array';
 
-export default class SortingVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { array: [] }; // So, whenever our react app starts, this array get initialized.
-  }
+const ArrayChart = () => {
+  const { array, swaps, comparisons } = useGlobalContext();
 
-  resetArray() {
-    const array = genrateRandomArray();
-    this.setState({ array });
-  }
-
-  componentDidMount() {
-    // This method is called during the mounting phase i.e after the components are rendered.
-    this.resetArray(); // Everytime we reload, we get a new Array
-  }
-
-  render() {
-    const { array } = this.state;
-    return (
-      <>
-        <div className="array-box">
-          {array.map((value, idx) => (
-            <div
-              className="array-bar"
-              id={`div-${idx}`}
-              key={idx}
-              style={{ height: `${value}px` }}
-            ></div>
-          ))}
-        </div>
-        <div className="array-controller">
-          <button className="try" onClick={() => this.resetArray()}>
-            Generate New Array
-          </button>
-          <button>Change Size</button>
-          <button>Change Speed</button>
-          <button>Old Array</button>
-        </div>
-      </>
+  return (
+    <>
+      <div className='array-box'>
+        {array.map((element, index) => {
+            return (
+              <div key={index} id={`div-${index}`} className='array-bar' style={{height: `${element}px`}}>
+                {element}
+              </div>
+            );
+        })}
+      </div>
+      <div>
+        comparisons: {comparisons}
+      </div>
+    </>
     );
-  }
-}
+};
+
+export { ArrayChart, createRandomArray };
