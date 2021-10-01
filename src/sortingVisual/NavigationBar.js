193c60868c2da9090035de1356256007f3d05a9c
be-jojo <67816573+be-jojo@users.noreply.github.com>
2021-10-01 20:06:47 +0530

    new start

--- src/sortingVisual/NavigationBar.js
+++ src/sortingVisual/NavigationBar.js
@@ -1,37 +1,50 @@
 import React from 'react';
-
-import "./ArrayChart.css";
-import {insertionSort} from './sortAlgorithms/insertion';
+import { FaBars } from 'react-icons/fa';
+import { Select, MenuItem } from '@material-ui/core';
+import { useGlobalContext } from './context';
+import { ArrayHandler } from './ArrayHandler';
+import './NavigationBar.css';
 
 function NavigationBar() {
+  const { algo, setAlgo } = useGlobalContext();
+  const updateAlgo = (e) => {
+    setAlgo(e.target.value)
+  }
   return (
     <nav>
-      <ul>
-        <li id='shortpath' className='items'>
-          <button className='buttons'>Shortest Path</button>
-        </li>
-        <li className='items'>
-          <button className='buttons'>Merge sort</button>
-        </li>
-        <li className='items'>
-          <button className='buttons'>Selection Sort</button>
-        </li>
-        <li className='items'>
-          <button className='buttons' onClick={insertionSort}>Insertion Sort</button>
-        </li>
-        <li className='items'>
-          <button className='buttons'>Bubble Sort</button>
-        </li>
-        <li className='items'>
-          <button className='buttons'>Quick sort</button>
-        </li>
-        <li className='items'>
-          <button className='buttons'>Shell sort</button>
-        </li>
-      </ul>
+      <div className='nav-center'>
+        <button className='nav-toggle'>
+          <FaBars/>
+        </button>
+      </div>
+      <ArrayHandler/>
+      <div className='select-algo'>
+        <Select
+          value={algo}
+          onChange={updateAlgo}
+        >
+          <MenuItem value={1}>
+            Insertion Sort
+          </MenuItem>
+          <MenuItem value={2}>
+            Bubble Sort
+          </MenuItem>
+          <MenuItem value={3}>
+            Bubble Sort
+          </MenuItem>
+          <MenuItem value={4}>
+            Merge Sort
+          </MenuItem>
+          <MenuItem value={5}>
+            Quick Sort
+          </MenuItem>
+          <MenuItem value={6}>
+            Heap Sort
+          </MenuItem>
+        </Select>
+      </div>
     </nav>
   )
 }
 
-export default NavigationBar
-
+export { NavigationBar }
